db82d3ec74a656942d28aa12cbb0a3186c25cef8
sunil pandey <sunil@sunils-MacBook-Pro.local>
2018-05-29 13:50:34 +0530

    changes done for role

--- src/app/models/dashboard.ts
+++ src/app/models/dashboard.ts
@@ -1,3 +1,3 @@
-export interface dashboard{
+export interface Dashboard{
     dashboard:any;
 }
--- src/app/models/filter.pipe.ts
+++ src/app/models/filter.pipe.ts
@@ -1,15 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
-  name: 'filter'
+    name: 'filter'
 })
 export class FilterPipe implements PipeTransform {
-    
-  transform(items: any[], searchText: string): any[] {
-    if(!items) return [];
-    if(!searchText) return items;
-searchText = searchText.toLowerCase();
-return items.filter( it => {
-      return it.toLowerCase().includes(searchText);
-    });
-   }
+
+    transform(items: any[], searchText: string): any[] {
+        if (!items) return [];
+        if (!searchText) return items;
+        searchText = searchText.toLowerCase();
+        return items.filter(it => {
+            return it.toLowerCase().includes(searchText);
+        });
+    }
 }
